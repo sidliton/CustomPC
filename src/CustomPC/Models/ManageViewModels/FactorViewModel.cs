bbd679b80efe723b971a1d48b7715b0ac4cb4760
Rafsanul Hasan <rafsan_14@hotmail.com>
2016-10-03 23:32:29 +0600

    Add project files.

---
+++ src/CustomPC/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomPC.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
